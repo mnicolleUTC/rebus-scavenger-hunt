1e3420e61799d1f0b957b482170f94345511c771
Mathieu Nicolle <mnicolle.utc@gmail.com>
2025-05-04 20:09:12 +0200

    🐛 fix deprecated param

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,5 @@ st.title("Hello, Emma!")
 
 st.image(
     "https://www.mmv.fr/images/cms/paysage-montagne-ete/paysage-montagne-mont-blanc.jpg?frz-v=535",
-    caption="Vue sur le Mont Blanc",
-    use_column_width=True
+    caption="Vue sur le Mont Blanc"
 )
