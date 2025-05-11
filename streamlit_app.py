763d29d5c0d0c777a389f44a53e158193efe5e3f
Mathieu Nicolle <mnicolle.utc@gmail.com>
2025-05-11 15:20:33 +0200

    ✨ Add images for scavanger hunt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,36 @@
 import streamlit as st
 
-st.title("Hello, Emma!")
+st.title("Voici ton rébus ! 👰‍♀️")
 
 
 st.image(
-    "https://www.mmv.fr/images/cms/paysage-montagne-ete/paysage-montagne-mont-blanc.jpg?frz-v=535",
-    caption="Vue sur le Mont Blanc"
+    "https://www.univers-musique.fr/wp-content/uploads/2017/04/partition-piano.png"
+)
+
+st.image(
+    "https://media.istockphoto.com/vectors/woman-in-trouble-with-extreme-sweating-constitution-sweat-that-does-vector-id1314001476?k=20&m=1314001476&s=612x612&w=0&h=G3QHn3QnApdUSrzXpGb00yrzrH3ZIeC7xys8FduKkpw="
+)
+
+st.image(
+    "https://static.vecteezy.com/system/resources/previews/017/399/394/original/a-pair-of-ace-playing-poker-cards-on-white-background-illustration-of-two-aces-winning-aces-vector.jpg"
+)
+
+st.image(
+    "https://i.ytimg.com/vi/R2njjO007_I/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGEQgZShDMA8=&rs=AOn4CLDfv74j-JIOrH3cFSwMvXtEJ8doHQ"
+)
+
+st.image(
+    "https://www.techowns.com/wp-content/uploads/2022/04/Pi-Symbol-on-Keyboard.jpg"
+)
+
+st.image(
+    "https://c8.alamy.com/compfr/2fkdx60/illustration-vectorielle-de-dessin-anime-de-scie-a-main-isolee-2fkdx60.jpg"
+)
+
+st.image(
+    "https://c8.alamy.com/compfr/2fkdx60/illustration-vectorielle-de-dessin-anime-de-scie-a-main-isolee-2fkdx60.jpg"
+)
+
+st.image(
+    "https://thumbs.dreamstime.com/z/rope-knot-vector-rope-knot-vector-eps-146466384.jpg"
 )
